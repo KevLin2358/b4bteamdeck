7b03303c8b6703a6cd30de4c4be32fe2a8753923
Kevin Lin <klin2358@gmail.com>
2022-06-21 16:16:14 -0400

    Added express routes for cards, decks and users

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
+const users = require("./routes/api/users");
+const decks = require("./routes/api/decks");
+const cards = require("./routes/api/cards");
+
 
 mongoose.connect(db,{useNewUrlParser: true})
 .then(() => console.log("Connected to mongoDB"))
@@ -10,8 +14,14 @@ mongoose.connect(db,{useNewUrlParser: true})
 
 
 app.get("/", (req, res) => {
+
   res.send("Hello World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/decks", decks);
+app.use("/api/cards", cards);
+
 
 const port = process.env.PORT || 5001;
 
