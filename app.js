cdb5ab77de6810fd560d99609420be5f7e5f011d
Kevin Lin <klin2358@gmail.com>
2022-06-21 16:50:25 -0400

    User model and test save for mongodb

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
 const users = require("./routes/api/users");
+const User = require("./models/User");
+
 const decks = require("./routes/api/decks");
 const cards = require("./routes/api/cards");
 
-
-mongoose.connect(db,{useNewUrlParser: true})
-.then(() => console.log("Connected to mongoDB"))
-.catch(err => console.log(err));
-
+mongoose
+  .connect(db,{useNewUrlParser: true})
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-
+  const user = new User({
+    handle: "Kev#4156",
+    username: "kev",
+    password: "kevkev"
+  })
+  user.save();
   res.send("Hello World!");
 });
 
 app.use("/api/users", users);
 app.use("/api/decks", decks);
 app.use("/api/cards", cards);
-
 
 const port = process.env.PORT || 5001;
 
